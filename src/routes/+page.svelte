1c10d0f486ca46013e39849552dc6a4009eb5e2a
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-09-23 11:36:17 +0200

    Added Here Maps functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
+<script>
+	import { onMount } from 'svelte';
+	let platform;
+	var mapElement;
+	var hereMap;
+	onMount(async () => {
+		// @ts-ignore
+		var H = window.H
+
+		// @ts-ignore
+		var platform = new H.service.Platform({
+			apikey: 'Zq0QbZGKiWBcdNsm5bgluVcs2vWxsPqaD2vv96W76sk'
+		});
+		// @ts-ignore
+		var defaultLayers = platform.createDefaultLayers();
+		// @ts-ignore
+		hereMap = new H.Map(mapElement, defaultLayers.vector.normal.map, {
+			center: {
+				lat: 50,
+				lng: 5
+			},
+			zoom: 4,
+			pixelRatio: window.devicePixelRatio || 1
+		});
+		var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(hereMap));
+		// @ts-ignore
+		var ui = H.ui.UI.createDefault(hereMap, defaultLayers);
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="map" bind:this={mapElement} />
+
+<style>
+	#map {
+		width: 95%;
+		height: 800px;
+		background: grey;
+	}
+</style>
